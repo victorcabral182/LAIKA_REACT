d39bef8dd8fa6ca5197a0d6e8c2937e7a5b9fd66
FillDias <79338600+FillDias@users.noreply.github.com>
2023-11-17 19:36:21 -0300

    Melhorias na navegação entre Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,20 +2,22 @@ import Link from "next/link";
 import React, { useState } from "react";
 import dog from "../../assets/imagens/dog.png";
 import Image from "next/image";
-
-export const Menu = () => {
+interface MenuProps {
+  fecharMenu: () => void;
+}
+export const Menu = ({ fecharMenu }: MenuProps) => {
   return (
     <div className="flex flex-col justify-center p-[42px] w-[100vw] h-[calc(100vh-75px)] fixed top-[75px] bg-[#E8E7E7]  z-10 gap-[64px] ">
-      <Link className="text-[24px]" href={""}>
+      <Link onClick={fecharMenu} className="text-[24px]" href={"/"}>
         HOME
       </Link>
-      <Link className="text-[24px]" href={""}>
+      <Link onClick={fecharMenu} className="text-[24px]" href={"/videos"}>
         VIDEOS
       </Link>
-      <Link className="text-[24px]" href={""}>
+      <Link onClick={fecharMenu} className="text-[24px]" href={""}>
         MERCH
       </Link>
-      <Link className="text-[24px]" href={""}>
+      <Link onClick={fecharMenu} className="text-[24px]" href={""}>
         TOUR
       </Link>
       <Image className="absolute right-[42px]" src={dog} alt="dog" />
