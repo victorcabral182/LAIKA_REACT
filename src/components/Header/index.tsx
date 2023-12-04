bdbff137a4d4c3e5e472fbe7385258e3b9614a9f
FillDias <79338600+FillDias@users.noreply.github.com>
2023-12-04 19:09:24 -0300

    link direcionando para a página oficial do Instagram

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,8 +13,8 @@ export const Header = () => {
 
   return (
     <>
-      <header className="w-full h-[75px] bg-[#E8E7E7] flex justify-between items-center px-2 border border-opacity-10 border-b-[#000]">
-        <Image src={logo} alt="laika" />
+      <header className="w-full h-[75px] bg-[#E8E7E7] flex justify-between items-center px-4 md:px-8  border border-opacity-10 border-b-[#000] ">
+        <Image src={logo} alt="laika" className="" />
         {!aberto ? (
           <AiOutlineMenu
             onClick={() => {
